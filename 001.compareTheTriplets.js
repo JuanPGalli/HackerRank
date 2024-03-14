23d85447d4018cf5850658b80f60248e826ac8e8
JuanPGalli <jpgallir@gmail.com>
2024-03-13 23:04:02 -0300

    problem solved

--- 001.compareTheTriplets.js
+++ 001.compareTheTriplets.js
@@ -16,7 +16,7 @@
 
 // a = [1, 2, 3]
 // b = [3, 2, 1]
-// For elements *0*, Bob is awarded a point because a[0] .
+// For elements *0*, Bob is awarded a point because a[0] < b[0].
 // For the equal elements a[1] and b[1], no points are earned.
 // Finally, for elements 2, a[2] > b[2] so Alice receives a point.
 // The return array is [1, 1] with Alice's score first and Bob's second.
@@ -29,11 +29,11 @@
 
 // int a[3]: Alice's challenge rating
 // int b[3]: Bob's challenge rating
+
 // Return
-
 // int[2]: Alice's score is in the first position, and Bob's score is in the second.
-// Input Format
 
+// Input Format
 // The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
 // The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
 
@@ -41,15 +41,15 @@
 
 // 1 ≤ a[i] ≤ 100
 // 1 ≤ b[i] ≤ 100
-// Sample Input 0
 
+// Sample Input 0
 // 5 6 7
 // 3 6 10
+
 // Sample Output 0
-
 // 1 1
-// Explanation 0
 
+// Explanation 0
 // In this example:
 // - a = (a[0],a[1],a[2]) = (5,6,7)
 // - b = (b[0],b[1],b[2]) = (3,6,10)
@@ -62,14 +62,13 @@
 // Alice's comparison score is 1, and Bob's comparison score is 1. Thus, we return the array [1,1].
 
 // Sample Input 1
-
 // 17 28 30
 // 99 16 8
+
 // Sample Output 1
 // 2 1
 
 // Explanation 1
-
 // Comparing the 0th. elements, 17<99 so Bob receives a point.
 // Comparing the 1st. and 2nd. elements, 28>16 and 30>8 so Alice receives two points.
 // The return array is [2,1].
@@ -86,5 +85,15 @@
  */
 
 function compareTriplets(a, b) {
-
+  let alice = 0;
+  let bob = 0;
+  for (let i = 0; i < a.length; i++){
+    if (a[i] !== b[i]) {
+      if(a[i] > b[i]) alice = alice + 1
+      else bob = bob + 1
+      
+    } 
+  }
+  return [alice, bob];
 }
+compareTriplets([17, 28, 30], [99, 16, 8])
