7c0db6a2722ac60ceaaef2b1db02335937b7667d
JuanPGalli <jpgallir@gmail.com>
2024-04-04 00:30:59 -0300

    BreakingTheRecords

--- 014.BreakingTheRecords.js
+++ 014.BreakingTheRecords.js
@@ -76,5 +76,20 @@ She broke her best record four times (after games 1, 2, 3, and 9) and her worst 
 
 function breakingRecords(scores) {
     // Write your code here
-  
+  let count = [0,0];
+  let score = [scores[0], scores[0]]
+  for (let i = 1; i < scores.length; i++){
+    if (scores[i] > score[0]) {
+      score[0] = scores[i];
+      count[0] = count[0] + 1;
+    } 
+    else if (scores[i] < score[1]) {
+      score[1] = scores[i];
+      count[1] = count[1] + 1;
+    }
+  }
+  return count;
 }
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
+
